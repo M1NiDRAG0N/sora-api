4725b1ac48e183b48d80bb571c105fbd612d7210
MINIDRAGON <minidragon.lee@gmail.com>
2026-02-09 16:09:20 +0900

    FIX : File Entity fixed

--- src/main/java/com/scit/soragodong/domain/entity/FileGrp.java
+++ src/main/java/com/scit/soragodong/domain/entity/FileGrp.java
@@ -1,32 +1,26 @@
 package com.scit.soragodong.domain.entity;
 
-import java.time.LocalDateTime;
-
-import org.springframework.data.annotation.CreatedDate;
-
+import com.scit.soragodong.common.BaseEntity;
 import com.scit.soragodong.domain.enums.FileRefType;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "FILE_GRP")
 @Getter
-@NoArgsConstructor
-public class FileGrp {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "FILE_GRP_IDX")
-    private Integer fileGrpIdx;
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class FileGrp extends BaseEntity {
 
     @Enumerated(EnumType.STRING)
     @Column(name = "REF_TYPE", nullable = false)
@@ -35,12 +29,4 @@ public class FileGrp {
     @Column(name = "REF_IDX", nullable = false)
     private Integer refId;
 
-    @CreatedDate
-    @Column(name = "CREATED_DT")
-    private LocalDateTime createdAt = LocalDateTime.now();
-
-    public FileGrp(FileRefType refType, Integer refId) {
-        this.refType = refType;
-        this.refId = refId;
-    }
 }
