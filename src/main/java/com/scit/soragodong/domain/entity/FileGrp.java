16ae805866f9b3bfb1060021814d3f1fb1345c73
MINIDRAGON <minidragon.lee@gmail.com>
2026-02-09 16:18:36 +0900

    ADD : Enhance File and FileGrp entities with additional fields

--- src/main/java/com/scit/soragodong/domain/entity/FileGrp.java
+++ src/main/java/com/scit/soragodong/domain/entity/FileGrp.java
@@ -1,5 +1,9 @@
 package com.scit.soragodong.domain.entity;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+
 import com.scit.soragodong.common.BaseEntity;
 import com.scit.soragodong.domain.enums.FileRefType;
 
@@ -7,6 +11,10 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -20,7 +28,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
-public class FileGrp extends BaseEntity {
+public class FileGrp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "FILE_GRP_IDX")
+    private Integer fileGrpIdx;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "REF_TYPE", nullable = false)
@@ -28,5 +41,14 @@ public class FileGrp extends BaseEntity {
 
     @Column(name = "REF_IDX", nullable = false)
     private Integer refId;
+
+    @CreatedDate
+    @Column(name = "CREATED_AT")
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
 
 }
